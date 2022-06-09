bf691badd5e1ac2f12e3cf5573e33e44c7076c26
bunsung92 <bunsung92@naver.com>
2022-06-09 19:30:11 +0900

    [#0] 깃 이그노어 추가 및 스프링 초기환경 설정

---
+++ MiniDoorayClient/src/main/java/com/nhnacademy/minidoorayclient/MiniDoorayClientApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidoorayclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDoorayClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniDoorayClientApplication.class, args);
+    }
+
+}
---
+++ MiniDoorayClient/src/test/java/com/nhnacademy/minidoorayclient/MiniDoorayClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidoorayclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDoorayClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
